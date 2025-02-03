7b655d05ecb1304543e4ab1a70130cedec349b2e
MarwahManan <marwahkhan671@gmail.com>
2025-02-04 04:47:24 +0500

    Update page.tsx

--- src/app/giftcard/page.tsx
+++ src/app/giftcard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Image from 'next/image';
 import { motion, AnimatePresence } from "framer-motion";
 import { FaGift, FaCreditCard, FaCheckCircle } from "react-icons/fa";
 
